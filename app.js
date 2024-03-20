30876fc60e3f3ea9c75b252dd7c360cbf6104d87
put your github username here <muathmhawich1@gmail.com>
2024-03-21 02:08:03 +0300

    add alert to the page

---
+++ app.js
@@ -0,0 +1,28 @@
+const Name=window.prompt('Enter your name please?',"your name");
+let gender =window.prompt("please enter your gender","male");
+let age=parseInt(window.prompt("please enter your age"));;
+do {
+
+    if(age<=0 || isNaN(age)){
+        age=window.prompt("please enter your  currect age more than zero ");
+    }
+      
+
+}while(age<=0||isNaN(age))
+let confirm=window.confirm('do you want to skip the welcoming message ? ');
+
+
+if(!confirm){
+    if(gender.startsWith('male')){
+        window.alert(`welcom Mr ${Name} in  your Todo list !!`)
+    }
+    else if(gender.startsWith('female')){
+         window.alert(`welcom Ms ${Name} in  your Todo list !!`)
+    }
+    else{
+        window.alert(`welcom  ${Name} in  your Todo list !!`)
+    }
+}
+
+//document.getElementById('todo').innerHTML="hello world"
+//document.getElementById('todo').style.fontSize='5px';
